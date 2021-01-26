3724d5b3cfc931e398113da3cdcc165211c1ca74
raymondboswel <raymondboswel@gmail.com>
2021-01-26 16:32:35 +0200

    Added ionic button to message room view

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,5 +19,34 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import LiveSocket from "phoenix_live_view";
 
-let liveSocket = new LiveSocket("/live", Socket);
+let Hooks = {};
+Hooks.Messages = {
+  updated() {
+    const ionApp = document.getElementById("ion-app");
+    console.log(ionApp);
+    ionApp.classList = "ion-page hydrated md";
+
+    console.log("hook executed");
+
+    document.getElementsByTagName("ion-button")[0].classList =
+      "hydrated md button button-solid ion-activatable ion-focusable";
+
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.src = `https://cdn.jsdelivr.net/npm/@ionic/core/loader/index.es2017.mjs?cachebuster='${new Date().getTime()}`;
+    script.innerText = `import { defineCustomElements } from 'https://cdn.jsdelivr.net/npm/@ionic/core/loader/index.es2017.mjs'
+      defineCustomElements();`;
+    // var script = document.createElement("script");
+    // script.src = `https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js?cachebuster='${new Date().getTime()}`;
+    // var script1 = document.createElement("script");
+    // script1.src = `https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js?cachebuster='${new Date().getTime()}`;
+    // script1.type = "module";
+    head.appendChild(script);
+
+    // head.appendChild(script1);
+    console.log("scripts added");
+  },
+};
+
+let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks });
 liveSocket.connect();
--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :lv_demo, LvDemo.Repo,
-  username: "raymond",
-  password: "password",
+  username: "testuser",
+  password: "letmein",
   database: "lv_demo_dev",
   hostname: "localhost",
   pool_size: 10
