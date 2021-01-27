0fb392daca4bb8f6b920def9640056a3676bf002
raymondboswel <raymondboswel@gmail.com>
2021-01-27 06:40:57 +0200

    Removed reference to secrets file

--- config/prod.exs
+++ config/prod.exs
@@ -66,6 +66,11 @@ config :logger, level: :info
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
 
-# Finally import the config/prod.secret.exs which should be versioned
-# separately.
-import_config "prod.secret.exs"
+
+# Configure your database
+config :lv_demo, LvDemo.Repo,
+  username: "${DB_USERNAME}"
+  password: "${DB_PASSWORD}",
+  database: "${DB_DATABASE}",
+  hostname: "${HOSTNAME}",
+  pool_size: 10
